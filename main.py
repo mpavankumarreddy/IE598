4e266f421b934675f51b43f58f7e509ec91ea3fe
mpavankumarreddy <m.pavankumarreddy@hotmail.com>
2015-05-01 02:08:14 +0100

    Updating estimating p_j for workers

--- main.py
+++ main.py
@@ -51,22 +51,136 @@ class Worker(object):
         self.worker_id = worker_id
 
 
-tasks = [Task(i, gen_t_i()) for i in range(n)]
-
-# A sample belief propagation on a random l-r
-l = 8       # l - r random graph
-r = 16      # l - r random graph
-
-m = int((n*l) / r)    # a free parameter
-
-worker_reliabilities = gen_betarnd(m)   # Note that this is a m x 1 matrix
-#workers = [Worker(reliability) for reliability in worker_reliabilities]
-workers = [Worker(i, worker_reliabilities[i][0]) for i in range(m)]
 
 
-G = nx.Graph()
-G.add_nodes_from(tasks)
-G.add_nodes_from(workers)
+
+
+
+
+
+
+
+
+
+
+
+# bp update equations
+
+def worker_to_task(G, tasks, workers):
+    # one bp loop - later incorporate into a while loop
+    #print 'worker to task'
+    for e in G.edges_iter(data=True):
+        # worker to task message updates
+
+        worker_j = workers[e[2]['w_id']]
+
+        adj_task_nodes = [out_edge['t_id'] for out_edge in G[worker_j].values()]
+        adj_task_nodes.remove(e[2]['t_id'])
+
+        vkjs = [G[tasks[i]][worker_j]['msg_t_w'] for i in adj_task_nodes]
+        akjs = [G[tasks[i]][worker_j]['Aij'] for i in adj_task_nodes]
+
+        for i in range(domain_size_p):
+            p = domain_p[i]
+            psi_k_j = [numpy.array([1-p, p])[::akj] for akj in akjs]
+            e[2]['msg_w_t'][i] = (numpy.prod([numpy.dot(vkjs[k], psi_k_j[k]) for k in range(len(adj_task_nodes))]))
+
+        # normalising back to 1
+        e[2]['msg_w_t'] /= sum(e[2]['msg_w_t'])
+
+
+def task_to_worker(G, tasks, workers):
+    #print 'task to worker'
+    for e in G.edges_iter(data=True):
+        # task to worker message updates
+
+        task_i = tasks[e[2]['t_id']]
+
+        adj_worker_nodes = [out_edge['w_id'] for out_edge in G[task_i].values()]
+        adj_worker_nodes.remove(e[2]['w_id'])
+
+        vkis = [G[workers[k]][task_i]['msg_w_t'] for k in adj_worker_nodes]
+        aiks = [G[workers[k]][task_i]['Aij'] for k in adj_worker_nodes]
+
+
+        for i in range(domain_size_t):
+            ti = domain_t[i]
+
+            # If they are equal, 1 else -1
+            e[2]['msg_t_w'][i] = numpy.prod([numpy.dot(t_vals[(int(ti == aiks[l]))], vkis[l]) for l in range(len(adj_worker_nodes))])
+
+        # normalising back to 1
+        e[2]['msg_t_w'] /= sum(e[2]['msg_t_w'])
+
+
+def estimate_ti(G, tasks, workers):
+    t_hats = []
+    for i in range(len(tasks)):
+        t_hat_i = []
+
+        task_i = tasks[i]
+
+        adj_worker_nodes = [out_edge['w_id'] for out_edge in G[task_i].values()]
+
+        vkis = [G[workers[k]][task_i]['msg_w_t'] for k in adj_worker_nodes]
+        aiks = [G[workers[k]][task_i]['Aij'] for k in adj_worker_nodes]
+
+        for i in range(domain_size_t):
+            ti = domain_t[i]
+
+            # If they are equal, 1 else -1
+            t_hat_i.append(numpy.prod([numpy.dot(t_vals[(int(ti == aiks[l]))], vkis[l]) for l in range(len(adj_worker_nodes))]))
+
+        sum_t = sum(t_hat_i)
+        t_hats.append(t_hat_i)
+
+    return t_hats
+
+
+def estimate_pj(G, tasks, workers):
+    w_hats = []
+    for j in range(len(workers)):
+        w_hat_j = []
+        worker_j = workers[j]
+        adj_task_nodes = [out_edge['t_id'] for out_edge in G[worker_j].values()]
+        vkjs = [G[tasks[i]][worker_j]['msg_t_w'] for i in adj_task_nodes]
+        akjs = [G[tasks[i]][worker_j]['Aij'] for i in adj_task_nodes]
+        for i in range(domain_size_p):
+            p = domain_p[i]
+            psi_k_j = [numpy.array([1-p, p])[::akj] for akj in akjs]
+            w_hat_j.append(numpy.prod([numpy.dot(vkjs[k], psi_k_j[k]) for k in range(len(adj_task_nodes))]) * dist_p[i])
+        sum_w = sum(w_hat_j)
+        w_hat_j = [temp/sum_w for temp in w_hat_j]
+        w_hats.append(w_hat_j)
+    return w_hats
+
+
+def evaluate_ti_estimates(G, tasks, workers):
+    # estimating task labels
+    t_hats = estimate_ti(G, tasks, workers)
+    t_est = numpy.array([t_h.index(max(t_h))*2 - 1 for t_h in t_hats])
+
+    t_act = numpy.array([task.label for task in tasks])
+    print "num correct - " + str(sum(t_est == t_act)) + " out of " + str(len(t_est))
+    return (t_est, t_act)
+
+
+def evaluate_wj_estimates(G, tasks, workers):
+    # estimating worker reliability
+    w_hats = estimate_pj(G, tasks, workers)
+    w_est = numpy.array([domain_p[w_h.index(max(w_h))] for w_h in w_hats])
+
+    w_act = numpy.array([worker.reliability for worker in workers])
+
+
+
+
+
+
+
+
+
+# methods for generating a random l-r graph
 
 def random_arr_permute(arr):
     '''
@@ -101,14 +215,34 @@ def configuration_model_connect(G, n, m, l, r):
                 aij = -task.label
             G.add_edge(worker, task, msg_t_w = None, msg_w_t = None, Aij = aij, t_id = nl[j], w_id = mr[j])
 
+
+tasks = [Task(i, gen_t_i()) for i in range(n)]
+
+# A sample belief propagation on a random l-r
+l = 8       # l - r random graph
+r = 16      # l - r random graph
+
+m = int((n*l) / r)    # a free parameter
+
+worker_reliabilities = gen_betarnd(m)   # Note that this is a m x 1 matrix
+#workers = [Worker(reliability) for reliability in worker_reliabilities]
+workers = [Worker(i, worker_reliabilities[i][0]) for i in range(m)]
+
+
+G = nx.Graph()
+G.add_nodes_from(tasks)
+G.add_nodes_from(workers)
+
 print 'Generating random graph here'
 configuration_model_connect(G, n, m, l, r)
 print 'Done generating graph'
 
 # A simple quantization mechanism to get a discrete probability distribution over the beta distribution
 
-domain_size_p = 8 # number of points in the discrete distribution
-domain_p = numpy.linspace(0, 1, domain_size_p + 2)[1:-1]
+domain_size_p = 20 # number of points in the discrete distribution
+
+domain_p = numpy.concatenate((numpy.linspace(0, 0.5, int(domain_size_p*0.2))[1:], numpy.linspace(0.5, 1, int(0.8*domain_size_p))[1:-1]))
+domain_size_p = len(domain_p)
 
 dist_p = numpy.array([beta.pdf(i, param_alpha, param_beta) for i in domain_p])
 dist_p = dist_p / sum(dist_p)
@@ -125,129 +259,73 @@ t_vals = [t_ne, t_eq]
 
 
 # should we make messages log? Lets see, how big they are after first iteration
-
-for e in G.edges_iter(data=True):
-    # update msg_t_w to be lookup table of 2 value and initialise these messages to 1
-    # WARNING: here [1, 1] corresponds to -1 and 1 values of ti. Didnot make it a dict for performance reasons
-    e[2]['msg_t_w'] = numpy.array([1.0/2, 1.0/2])
-    # update msg_w_t to be lookup table of domain_size_p and no initialisation required here
-    # sorted keys order
-    e[2]['msg_w_t'] = numpy.array([1.0/domain_size_p]*domain_size_p)
-
-
-def worker_to_task():
-    # one bp loop - later incorporate into a while loop
-    #print 'worker to task'
+def init_graph(G):
     for e in G.edges_iter(data=True):
-        # worker to task message updates
+        # update msg_t_w to be lookup table of 2 value and initialise these messages to 1
+        # WARNING: here [1, 1] corresponds to -1 and 1 values of ti. Didnot make it a dict for performance reasons
+        e[2]['msg_t_w'] = numpy.array([1.0/domain_size_t]*domain_size_t)
+        # update msg_w_t to be lookup table of domain_size_p and no initialisation required here
+        # sorted keys order
+        e[2]['msg_w_t'] = numpy.array([1.0/domain_size_p]*domain_size_p)
 
-        worker_j = workers[e[2]['w_id']]
-
-        adj_task_nodes = [out_edge['t_id'] for out_edge in G[worker_j].values()]
-        adj_task_nodes.remove(e[2]['t_id'])
-
-        vkjs = [G[tasks[i]][worker_j]['msg_t_w'] for i in adj_task_nodes]
-        akjs = [G[tasks[i]][worker_j]['Aij'] for i in adj_task_nodes]
-
-        for i in range(domain_size_p):
-            p = domain_p[i]
-            psi_k_j = [numpy.array([1-p, p])[::akj] for akj in akjs]
-            e[2]['msg_w_t'][i] = (numpy.prod([numpy.dot(vkjs[k], psi_k_j[k]) for k in range(len(adj_task_nodes))]))
-
-        # normalising back to 1
-        e[2]['msg_w_t'] /= sum(e[2]['msg_w_t'])
+init_graph(G)
 
 
-def task_to_worker():
-    #print 'task to worker'
-    for e in G.edges_iter(data=True):
-        # task to worker message updates
-
-        task_i = tasks[e[2]['t_id']]
-
-        adj_worker_nodes = [out_edge['w_id'] for out_edge in G[task_i].values()]
-        adj_worker_nodes.remove(e[2]['w_id'])
-
-        vkis = [G[workers[k]][task_i]['msg_w_t'] for k in adj_worker_nodes]
-        aiks = [G[workers[k]][task_i]['Aij'] for k in adj_worker_nodes]
 
 
-        for i in range(domain_size_t):
-            ti = domain_t[i]
-
-            # If they are equal, 1 else -1
-            e[2]['msg_t_w'][i] = numpy.prod([numpy.dot(t_vals[(int(ti == aiks[l]))], vkis[l]) for l in range(len(adj_worker_nodes))])
-
-        # normalising back to 1
-        e[2]['msg_t_w'] /= sum(e[2]['msg_t_w'])
 
 
-def estimate_ti():
-    t_hats = []
-    for i in range(len(tasks)):
-        t_hat_i = []
-
-        task_i = tasks[i]
-
-        adj_worker_nodes = [out_edge['w_id'] for out_edge in G[task_i].values()]
-
-        vkis = [G[workers[k]][task_i]['msg_w_t'] for k in adj_worker_nodes]
-        aiks = [G[workers[k]][task_i]['Aij'] for k in adj_worker_nodes]
-
-        for i in range(domain_size_t):
-            ti = domain_t[i]
-
-            # If they are equal, 1 else -1
-            t_hat_i.append(numpy.prod([numpy.dot(t_vals[(int(ti == aiks[l]))], vkis[l]) for l in range(len(adj_worker_nodes))]))
-
-        t_hats.append(t_hat_i)
-
-    return t_hats
-
-def estimate_pj():
-    w_hats = []
-    for j in range(len(workers)):
-        w_hat_j = []
-
-        worker_j = workers[j]
-
-        adj_task_nodes = [out_edge['t_id'] for out_edge in G[worker_j].values()]
-
-        vkjs = [G[tasks[i]][worker_j]['msg_t_w'] for i in adj_task_nodes]
-        akjs = [G[tasks[i]][worker_j]['Aij'] for i in adj_task_nodes]
-
-        for i in range(domain_size_p):
-            p = domain_p[i]
-            psi_k_j = [numpy.array([1-p, p])[::akj] for akj in akjs]
-            w_hat_j.append(numpy.prod([numpy.dot(vkjs[k], psi_k_j[k]) for k in range(len(adj_task_nodes))]))
 
 
-        w_hats.append(w_hat_j)
-
-    return w_hats
 
 
-def evaluate_ti_estimates():
-    # estimating task labels
-    t_hats = estimate_ti()
-    t_est = numpy.array([t_h.index(max(t_h))*2 - 1 for t_h in t_hats])
 
-    t_act = numpy.array([task.label for task in tasks])
-    print "num correct - " + str(sum(t_est == t_act)) + " out of " + str(len(t_est))
 
-def evaluate_wj_estimates():
-    # estimating worker reliability
-    w_hats = estimate_pj()
-    w_est = numpy.array([domain_p[w_h.index(max(w_h))] for w_h in w_hats])
 
-    w_act = numpy.array([worker.reliability for worker in workers])
+
+
 
 
 print 'before bp'
-evaluate_ti_estimates();
+evaluate_ti_estimates(G, tasks, workers);
 
-for i in range(4):
-    print 'iteration - ' + str(i)
-    worker_to_task();
-    task_to_worker();
-    evaluate_ti_estimates();
+w_act = numpy.array([worker.reliability for worker in workers])
+
+def print_results(l):
+    for i in range(l):
+        print 'iteration - ' + str(i)
+        worker_to_task(G, tasks, workers);
+        task_to_worker(G, tasks, workers);
+        evaluate_ti_estimates(G, tasks, workers)
+        w_hats = estimate_pj(G, tasks, workers)
+        w_est = numpy.array([domain_p[w_h.index(max(w_h))] for w_h in w_hats])
+
+        print "mean error in reliability estimates - " + str(1.0/1000*sum(abs(w_est - w_act)**1))
+        print "rms error in reliability estimates - " + str(numpy.sqrt(1.0/1000*sum(abs(w_est - w_act)**2)))
+
+print_results(3)
+
+
+# construct a new graph by pruning away some of the unreliable workers from the old graph
+w_hats = estimate_pj(G, tasks, workers)
+w_est = numpy.array([domain_p[w_h.index(max(w_h))] for w_h in w_hats])
+
+'''
+while min([G.degree(task) for task in tasks]) > 3:
+    remove_node_index = numpy.argmin(w_est)
+    G.remove_node(workers[remove_node_index])
+    w_est[remove_node_index] = float("inf")
+'''
+
+for i in range(len(tasks)):
+    task_i = tasks[i]
+    adj_worker_nodes = [out_edge['w_id'] for out_edge in G[task_i].values()]
+    while len(adj_worker_nodes) > 2:
+        unreliable_worker_index = numpy.argmin([w_est[k] for k in adj_worker_nodes])
+        G.remove_edge(task_i, workers[adj_worker_nodes[unreliable_worker_index]])
+        removed_worker = adj_worker_nodes.pop(unreliable_worker_index)
+
+init_graph(G)
+
+
+#sum(abs(numpy.array([G[t][worker]['Aij'] for t in G[worker]]) - numpy.array([t.label for t in G[worker]]))/2)
